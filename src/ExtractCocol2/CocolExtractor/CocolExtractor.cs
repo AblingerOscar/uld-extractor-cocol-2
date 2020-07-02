c5c09d9e8f204b10e754130c444e49fbc53646b9
Oscar Ablinger <dev.ablinger@gmail.com>
2020-07-02 19:31:21 +0200

    fix: also generate main file

---
+++ src/ExtractCocol2/CocolExtractor/CocolExtractor.cs
@@ -0,0 +1,22 @@
+﻿using DefinitionFileBuillder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public static class CocolExtractor
+{
+    public static DefinitionFileBuilder Builder { get; private set; }
+
+    public static void Main(string[] args)
+    {
+        Builder = new DefinitionFileBuilder();
+        Console.WriteLine("finished set-up");
+
+        Cocol2Extractor.Main(args);
+
+        Console.WriteLine("building file");
+        var langDef = Builder.Build();
+
+        Console.WriteLine(langDef);
+    }
+}
---
+++ src/ExtractCocol2/DefinitionFileBuillder/DefinitionFileBuilder.cs
@@ -0,0 +1,20 @@
+﻿using autosupport_lsp_server;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefinitionFileBuillder
+{
+    public class DefinitionFileBuilder
+    {
+        public DefinitionFileBuilder()
+        {
+
+        }
+
+        public IAutosupportLanguageDefinition Build()
+        {
+            return null;
+        }
+    }
+}
