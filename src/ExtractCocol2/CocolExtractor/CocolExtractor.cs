8611e4ba828614bf2bc2bf2bb956b587d6669a7a
Oscar Ablinger <dev.ablinger@gmail.com>
2020-07-05 19:44:20 +0200

    feat: implement rule extraction

--- src/ExtractCocol2/CocolExtractor/CocolExtractor.cs
+++ src/ExtractCocol2/CocolExtractor/CocolExtractor.cs
@@ -1,7 +1,8 @@
-﻿using DefinitionFileBuillder;
+﻿using autosupport_lsp_server;
+using autosupport_lsp_server.Symbols.Impl.Terminals;
+using DefinitionFileBuillder;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 public static class CocolExtractor
 {
@@ -12,11 +13,34 @@ public static class CocolExtractor
         Builder = new DefinitionFileBuilder();
         Console.WriteLine("finished set-up");
 
+
         Cocol2Extractor.Main(args);
+
 
         Console.WriteLine("building file");
         var langDef = Builder.Build();
 
-        Console.WriteLine(langDef);
+        Console.WriteLine($"While Building the file, there were {Builder.Errors.Count} error{(Builder.Errors.Count == 1 ? "" : "s")}");
+
+        foreach (var error in Builder.Errors)
+            Console.WriteLine('\t' + error);
+
+
+        Console.WriteLine("Verifying result…");
+        var errors = langDef.VerifyAndGetErrors();
+        if (errors.Length == 0)
+            Console.WriteLine("No errors found :)");
+        else
+            Console.WriteLine($"Found {errors.Length} error{(errors.Length == 1 ? "" : "s")}:");
+
+        foreach (var error in errors)
+            Console.WriteLine('\t' + error);
+
+        if (errors.Length != 0)
+            return;
+
+        Console.WriteLine("Printing Language Definition File to STDOUT:");
+        Console.WriteLine();
+        Console.WriteLine(langDef.SerializeToXLinq());
     }
 }
--- src/ExtractCocol2/DefinitionFileBuillder/DefinitionFileBuilder.cs
+++ src/ExtractCocol2/DefinitionFileBuillder/DefinitionFileBuilder.cs
@@ -1,12 +1,27 @@
 ﻿using autosupport_lsp_server;
+using autosupport_lsp_server.Symbols;
+using autosupport_lsp_server.Symbols.Impl;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DefinitionFileBuillder
 {
     public class DefinitionFileBuilder
     {
+        private bool nextCommentIsAction = false;
+        private StringBuilder currentComment = new StringBuilder();
+
+        private string languageId;
+        private string languageFilePattern;
+        private IList<CommentRule> documentationComments = new List<CommentRule>();
+        private IList<CommentRule> normalComments = new List<CommentRule>();
+        private IList<string> startRules = new List<string>();
+        private Dictionary<string, IRule> rules = new Dictionary<string, IRule>();
+
+        public IList<string> Errors = new List<string>();
+
         public DefinitionFileBuilder()
         {
 
@@ -14,7 +29,38 @@ namespace DefinitionFileBuillder
 
         public IAutosupportLanguageDefinition Build()
         {
-            return null;
+            CommentIsFinished();
+
+            return new AutosupportLanguageDefinition(
+                    languageId, languageFilePattern, commentRules: new CommentRules(normalComments.ToArray(), documentationComments.ToArray()), startRules.ToArray(), rules
+                );
+        }
+
+        public void NextCommentIsAction(bool isAction)
+        {
+            CommentIsFinished();
+            nextCommentIsAction = isAction;
+        }
+
+        public void AddCommentChar(char ch)
+            => currentComment.Append(ch);
+
+        public void CommentIsFinished()
+        {
+            // TODO
+            Console.WriteLine($"found finished {(nextCommentIsAction ? "action" : "non-action")} comment: <{currentComment}>");
+
+            currentComment.Clear();
+        }
+
+        public void StartOfRules()
+        {
+            CommentIsFinished();
+        }
+
+        public void AddRule(string ruleName, ISymbol[] symbols)
+        {
+            rules.Add(ruleName, new Rule(ruleName, symbols));
         }
     }
 }
